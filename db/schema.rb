919b867d085e0f6797deaa9c7be7f9b9e8c44901
flemdizzle <fleminsh@gmail.com>
2014-11-03 16:41:26 -0500

    added author to issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103211440) do
+ActiveRecord::Schema.define(version: 20141103214053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141103211440) do
     t.string   "issue_url"
     t.string   "comment"
     t.integer  "user_id"
+    t.string   "author"
   end
 
   create_table "repos", force: true do |t|
